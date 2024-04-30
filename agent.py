1e1b3de07e15d69158ab4a10bfecfa8f6276cafd
yyh_603(edu) <yiyang603.cs12@nycu.edu.tw>
2024-04-30 14:29:16 +0800

    add agent

---
+++ agent.py
@@ -0,0 +1,11 @@
+from game import Game
+
+class Agent:
+    def __init__():
+        pass
+    
+    def getAction(game: Game):
+        raise NotImplementedError()
+
+    def returnFaliedAction():
+        raise NotImplementedError()
