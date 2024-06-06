51f9172ee0fa179b162137f78852f40f703ce0ee
yyh_603(edu) <yiyang603.cs12@nycu.edu.tw>
2024-06-06 22:11:49 +0800

    add CoT Agent

--- IncontextAgent.py
+++ IncontextAgent.py
@@ -6,7 +6,7 @@ import re
 
 class IncontextAgent(Agent):
 
-    def process_response(self, response: str):
+    def _process_response(self, response: str):
         pattern =  r"REASON: (?P<reason>.*)\nACTION: (?P<action>open|flag) (?P<x>\d+) (?P<y>\d+)"
         match = re.match(pattern, response)
         if match:
@@ -20,27 +20,7 @@ class IncontextAgent(Agent):
         else:
             return False, None
 
-    def getAction(self, game: Game):
-        promptgen = IncontextPrompt()
-        prompt = ""
-        prompt += promptgen.intro_game()
-        prompt += promptgen.intro_cell_state()
-        prompt += promptgen.intro_actions()
-        prompt += promptgen.action_format()
-        prompt += promptgen.regulation()
-        prompt += promptgen.example_1()
-        prompt += promptgen.example_2()
-        prompt += promptgen.example_3()
-        prompt += promptgen.response_guide(game, None)
-        response = self.api.sendMessage(promptgen.system_message(), prompt)
-
-        valid, response = self.process_response(response)
-        if not valid:
-            return False, None, None
-        else:
-            return True, response["action"], (response["x"], response["y"])
-
-    def returnFalied(self, game: Game, falied_reason: ActionFeedback):
+    def getAction(self, game: Game, falied_reason: ActionFeedback):
         promptgen = IncontextPrompt()
         prompt = ""
         prompt += promptgen.intro_game()
@@ -54,7 +34,7 @@ class IncontextAgent(Agent):
         prompt += promptgen.response_guide(game, falied_reason)
         response = self.api.sendMessage(promptgen.system_message(), prompt)
 
-        valid, response = self.process_response(response)
+        valid, response = self._process_response(response)
         if not valid:
             return False, None, None
         else:
